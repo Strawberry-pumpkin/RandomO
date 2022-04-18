530ad6ab7da9e2b883281f9144f5ae699f5d9751
Strawberry-pumpkin <103160608+Strawberry-pumpkin@users.noreply.github.com>
2022-04-18 16:04:10 +0530

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "wert5yu9higtroel,dkmjcv"
+    app.config['SECRET_KEY'] =  "enteracustomadesecretkeyonceyoudownloadthis:)(*$URJKD"
     from .views import views
     app.register_blueprint(views, url_prefix='/')
     return app
